c6c1bbc0ea4cf62b59623adaca1928faaf84108e
gustavo <silvagustavo@yandex.ru>
2020-08-04 12:23:32 +0300

    Add web app

---
+++ src/pages/teacherList/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './styles.css'
+import PageHeader from "../../components/pageHeader";
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import TeacherItem from "../../components/teacherItem";
+
+
+function TeacherList() {
+    return (
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estao sao os proffys disponiveis.">
+                <form action="" id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Materia</label>
+                        <input type="text" id="subject"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="week-day">Dia da Semana</label>
+                        <input type="text" id="week-day"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" id="time"/>
+                    </div>
+                </form>
+            </PageHeader>
+
+            <main>
+                <TeacherItem/>
+            </main>
+        </div>
+    )
+}
+
+export default TeacherList
